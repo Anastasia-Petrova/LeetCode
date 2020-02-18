7f981919db08bce7006f0f7ddf77815ad1c3bdd9
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-18 12:35:28 +0000

    add Rotate Array

--- LeetCode.playground/Pages/Rotate Array .xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Rotate Array .xcplaygroundpage/Contents.swift
@@ -1,4 +1,47 @@
-import UIKit
+/*:
+* Callout(Task):
+Given an array, rotate the array to the right by k steps, where k is non-negative.
 
+```
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+```
+```
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+```
+*/
+
+func replaceXor(array: inout [Int], firstIndex : Int, secondIndex: Int) {
+    array[firstIndex] = array[firstIndex] ^ array[secondIndex]
+    array[secondIndex] = array[firstIndex] ^ array[secondIndex]
+    array[firstIndex] = array[firstIndex] ^ array[secondIndex]
+}
+
+func rotate(_ nums: inout [Int], _ k: Int) {
+    guard !nums.isEmpty else {
+        return
+    }
+    for _ in 0..<k {
+        for i in 0..<nums.count - 1 {
+            let firstIndex = nums.count - 1 - i
+            let secondIndex = firstIndex - 1
+            replaceXor(array: &nums, firstIndex: firstIndex, secondIndex: secondIndex)
+        }
+    }
+}
+
+import XCTest
+var input = [1,2,3,4,5,6,7]
+var k = 3
+rotate(&input, k)
+XCTAssertEqual(input, [5,6,7,1,2,3,4])
 
 
